59d86c9852b0157ccd5a98885d20eec1d81c3eb8
sumona <63995737+Meherjavienothoy@users.noreply.github.com>
2022-10-05 02:06:41 +1100

    first commit

--- fibonacciWithRecursive.js
+++ fibonacciWithRecursive.js
@@ -1,1 +1,15 @@
-//
+// recursive hocche funtion er vitore funtion k call kora
+//10 tomo poder maan show korbe
+function Fibonacci(n){
+    if(n == 0){
+        return 0;
+    }
+    if (n == 1){
+        return 1;
+    }
+    else{
+        return Fibonacci(n-1) + Fibonacci(n-2);
+    }
+}
+var result = Fibonacci(10);
+console.log(result);
