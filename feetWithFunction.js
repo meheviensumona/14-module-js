5cf4e2125ec74444fb2e5b1af001a5fae7e694e3
sumona <63995737+Meherjavienothoy@users.noreply.github.com>
2022-10-04 13:46:08 +1100

    first commit

---
+++ feetWithFunction.js
@@ -0,0 +1,8 @@
+// convert inch to Feet with (function)
+function  inchToFeet(num){
+    var result = num/12;
+    result = result.toFixed(2)
+    console.log( "your Height is:",result,"Feet")
+}
+
+inchToFeet(70);
