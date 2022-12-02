e59fca459b25ff55a233253e0d8d873a60fc031c
sumona <63995737+Meherjavienothoy@users.noreply.github.com>
2022-12-02 23:23:46 +1100

    2nd commit

--- checkPrimeNum.js
+++ checkPrimeNum.js
@@ -1,7 +1,14 @@
+console.log('exampl2-1')
+var n = 28;
 
-//example-1
-//vul ase code kaj kre nah
-var n = 4;
+  if (n===1)
+  {
+    console.log('this is not a prime number');
+  }
+  else if(n === 2)
+  {
+    console.log('this is a prime number')
+  }
 for (i =2; i<n; i++){
  
     if(n % i == 0){
